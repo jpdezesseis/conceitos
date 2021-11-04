9264153d6dbd4d3569704e8004d404bb7ca9228e
TREINAMENTO28 <TREINAMENTO28@TREINAMENTO28N>
2021-11-04 09:25:20 -0300

    tenstando meu git

---
+++ Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Conceitos
+{
+    public class Pessoa
+    {
+
+        public void apresentar()
+        {
+            Console.WriteLine("ola");
+        }
+        public void apresentar(string nome)
+        {
+            Console.WriteLine("ola" + nome);
+        }
+        public void apresentar(string nome, int idade)
+        {
+            Console.WriteLine("ola " + nome + " voce tem " + idade + " anos");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Conceitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.apresentar();
+            pessoa.apresentar("João");
+            pessoa.apresentar("João", 23);
+
+
+            Console.ReadLine();
+        }
+    }
+}
